c55bc2e3d4aa372f7b5edcfb775c8a74da72b3f0
KhoiPM23 <phanminhkhoi23@gmail.com>
2025-03-08 13:57:50 +0700

    Update config.js
    Adjustment

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Em';
+const nameGirl = 'Em Yêu';
 const giftUrl = '';
 const eventName = 'Chúc Mừng 8-3';
 const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const contentCard = 'Chúc Bé Hên của anh 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
 const giftImage = 'My_Diu_Hen.png';
 const base64 = '';
