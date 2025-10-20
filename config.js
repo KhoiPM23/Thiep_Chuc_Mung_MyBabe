301132fbb53e84a9e94b46551d382b4c1b705c6c
KhoiPM23 <phanminhkhoi23@gmail.com>
2025-10-20 13:01:10 +0700

    Update event name and card content for 20/10

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const nameGirl = 'Em Yêu';
 const giftUrl = '';
-const eventName = 'Chúc Mừng 8-3';
+const eventName = 'Chúc mừng 20/10';
 const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc Bé Hên của anh 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const contentCard = 'Cảm ơn vì đã là nguồn cảm hứng và yêu thương. Chúc bé Hên của anh một ngày 20/10 đầy nụ cười và hạnh phúc! ❤️\n— Khoi.PM';
 
-const giftImage = 'My_Diu_Hen.png';
+const giftImage = 'MyBabe.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
